673e1113a856bf2d180b23dffe83e7989b2b5078
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-07-17 20:37:54 +0530

    Defined interface, added cons, conj,concat

--- pysistence/ipersistent.py
+++ pysistence/ipersistent.py
@@ -1,2 +1,16 @@
-class IPersistent:
-    pass
+from abc import ABC, abstractmethod
+
+class IPersistent(ABC):
+    
+    @abstractmethod
+    def cons(self, value):
+        pass
+
+
+    @abstractmethod
+    def conj(self, value):
+        pass
+    
+    @abstractmethod
+    def concat(self, value):
+        pass
