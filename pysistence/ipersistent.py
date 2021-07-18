89ef1bcd404fd3288b0fd409a9c2b412879c36f0
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-07-18 16:45:11 +0530

    won't be considered by coverage

--- pysistence/ipersistent.py
+++ pysistence/ipersistent.py
@@ -1,15 +1,16 @@
 from abc import ABC, abstractmethod
 
-
-class IPersistent(ABC):
+class IPersistent(ABC): # pragma: no cover
+    
     @abstractmethod
     def cons(self, value):
         pass
 
+
     @abstractmethod
     def conj(self, value):
         pass
-
+    
     @abstractmethod
     def concat(self, value):
         pass
